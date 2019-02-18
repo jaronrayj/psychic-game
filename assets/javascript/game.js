43c1dd904fad0a92890e6981a31fb2d9c2cdb563
Jaron Johnson <jjaronjohnson14@gmail.com>
2019-02-18 08:45:14 -0700

    Logic works! Now to get it to display properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,18 +5,19 @@ var guessesLeftText = document.getElementById("guessesLeft-text");
 var guessesMadeText = document.getElementById("guessesMade-text");
 
 // Variables
-wins = 0;
-losses = 0;
-guessesLeft = 9;
-guessesMadeText = [];
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessesMade = ["r", "t"];
 
+console.log("Wins: " + wins);
 // Displayed text
-function dataLoad() {
-    winsText.textContent = "Wins: " + wins;
-    lossessText.textContent = "Losses: " + toString.losses;
-    guessesLeftText.textContent = "Number of guesses left: " + toString.guessesLeft;
-    guessesMadeText.textContent = "Guesses made: " + toString.guessesMadeText
-}
+
+// winsText.textContent = "Wins: " + wins;
+// lossessText.textContent = "Losses: " + losses;
+// guessesLeftText.textContent = "Number of guesses left: " + guessesLeft;
+// guessesMadeText.textContent = "Guesses made: " + guessesMade;
+
 
 // Generate random string
 function generate_random_string(string_length) {
@@ -35,18 +36,33 @@ console.log("computer guess " + compGuess);
 
 
 // Pull user's input
-document.onkeyup = function game(event) {
-    var userInput = event.key;
+document.onkeydown = function game(event) {
 
+    var userInput = event.key;
+    // console.log("My Guess " + userInput);
     // if guesses correctly, user wins
     if (userInput === compGuess) {
+
         wins++;
-        generate_random_string(1);
+        guessesLeft = 9;
+        compGuess = generate_random_string(1);
         console.log("users points: " + wins);
-        console.log(compGuess);
+        console.log("computer guess " + compGuess);
+
     } else {
         // If wrong guess, user gets 9 tries and then losses
-        (guessesLeft - 1);
-        console.log(guessesLeft);
+        if (guessesLeft > 1) {
+
+            guessesLeft--;
+            console.log("Guesses left: " + guessesLeft);
+
+        } else {
+            // After 9 guesses start over again with adding a loss and a new string
+            losses++;
+            compGuess = generate_random_string(1);
+            guessesLeft = 9;
+            console.log("losses: " + losses);
+            console.log("computer guess " + compGuess);
+        }
     }
 }
